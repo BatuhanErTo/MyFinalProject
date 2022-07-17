350bc1f616405acf1f230ea7e758dedac8423503
Batuhan Erol <bataer57@gmail.com>
2022-07-17 15:46:59 +0300

    Core layer added

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    // generic constraint
+    // class : reference type
+    // IEntity : it could be both IEntity and classes that implement IEntity
+    // new() : it must renewable
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    // IEntity implent eden class bir veritabını tablosudur yani bir veritabanı tablsoudur
+    // core katmanı başka bir katmanı referans almaz
+    public interface IEntity
+    {
+    }
+}
