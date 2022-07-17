5d2c25aaa4f74cd3e8dc14ad5891e67eb9ab7162
Batuhan Erol <bataer57@gmail.com>
2022-07-17 15:48:11 +0300

    Modified

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,10 +9,26 @@ namespace ConsoleUI // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());//solid => open closed principle
-            foreach (var product in productManager.GetAll())
+            //DTOs = Data Transformation Object
+            ProductTest();
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());//solid => open closed principle
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
         }
     }
