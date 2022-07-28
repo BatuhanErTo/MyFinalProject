f351020219955464c9e3d64172377ef51f9f2429
Batuhan Erol <bataer57@gmail.com>
2022-07-28 20:05:03 +0300

    API added

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        //IoC container ---> inversion of control
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
